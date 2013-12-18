4e31044dec823814a7bd59ff46f6f1b58a0b1b36
Derek Nassler <dnassler@rogers.com>
2013-12-18 16:33:36 -0500

    tweaked the getFeatured response to change the type of cupcake displayed

--- cloud/main.js
+++ cloud/main.js
@@ -2,5 +2,10 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success("Hello world! This is an interesting experiment, me thinks.");
+});
+Parse.Cloud.define("getFeatured", function(request, response) {
+	//response.success("chocolate");
+	//response.success("rainbow");
+	response.success("watermelon");
 });
